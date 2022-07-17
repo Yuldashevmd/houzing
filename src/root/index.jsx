dbe1bff3beafddd859df522d6403870d4027519a
yuldashev_md <muxammad_205@mail.ru>
2022-07-17 11:51:27 +0500

    ll

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../components/Navbar/index";
-import { Footer } from "../components/Footer/index";
+import Navbar from "../components/navbar/index";
+import { Footer } from "../components/footer/index";
 import {
   BrowserRouter as Router,
   Routes,
